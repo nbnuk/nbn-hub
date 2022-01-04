3d8765d5cb726092047da1f86888735dd9f0bedb
Helen Manders Jones <realhelenmjones@gmail.com>
2022-01-04 15:51:40 +0000

    bypass loading page for uptimerobot

--- grails-app/controllers/uk/org/nbn/biocache/hub/LoadingInterceptor.groovy
+++ grails-app/controllers/uk/org/nbn/biocache/hub/LoadingInterceptor.groovy
@@ -8,7 +8,8 @@ class LoadingInterceptor {
 
     boolean before() {
         //loading spinner is only supported for GET requests (for now). Most are GET
-        if (request.get && !params.get("nbn_loading")) {
+        String userAgent = request.getHeader("User-Agent");
+        if (request.get && !params.get("nbn_loading") && userAgent.indexOf("UptimeRobot")<0) {
             String url = request.requestURI+"?"+request.queryString+"&nbn_loading=true";
             render(view: "../loading", model:[url:url])
             return false
