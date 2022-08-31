641f4d2b4d53c246c2edc946d1589e81ec1d9a64
Helen Manders-Jones <realhelenmjones@gmail.com>
2022-08-31 09:34:43 +0100

    added an ALA fix

--- grails-app/controllers/uk/org/nbn/biocache/hubs/OccurrenceController.groovy
+++ grails-app/controllers/uk/org/nbn/biocache/hubs/OccurrenceController.groovy
@@ -7,8 +7,9 @@ class OccurrenceController extends au.org.ala.biocache.hubs.OccurrenceController
 
     @Override
     def list(SpatialSearchRequestParams requestParams) {
+        String[] fq = params.list("fq") as String[] //what ALA do to override Grails (handles fq=<empty string>)
 
-        if(requestParams.fq.length == 0 && grailsApplication.config.facets.defaultFilters){
+        if(fq.length == 0 && grailsApplication.config.facets.defaultFilters){
 //            requestParams.fq = grailsApplication.config.facets.defaultFilters.toString().split(',')
 //            requestParams.fq = ['occurrence_status:"present"', '-user_assertions:(50001 OR 50005 OR 50006)']
             params.put('fq',grailsApplication.config.facets.defaultFilters.toString().split(','));
