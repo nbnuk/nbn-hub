372e253f83b93fc65a154f7656b7503116ab8dce
Keith Raven <k.raven@nbn.org.uk>
2024-11-28 15:33:40 +0000

    Add exclusion pattern for default filters Primarily for advanced search by occurrence_id ATLASDEV-1633

--- grails-app/controllers/uk/org/nbn/biocache/hubs/OccurrenceController.groovy
+++ grails-app/controllers/uk/org/nbn/biocache/hubs/OccurrenceController.groovy
@@ -8,8 +8,10 @@ class OccurrenceController extends au.org.ala.biocache.hubs.OccurrenceController
     @Override
     def list(SpatialSearchRequestParams requestParams) {
         String[] fq = params.list("fq") as String[] //what ALA do to override Grails (handles fq=<empty string>)
+        String q = params.getOrDefault("q","")
+        String defaultFilterExclusionPattern = grailsApplication.config.facets.defaultFiltersQueryExclusionPattern
 
-        if(fq.length == 0 && grailsApplication.config.facets.defaultFilters){
+        if(fq.length == 0 && grailsApplication.config.facets.defaultFilters && !(defaultFilterExclusionPattern != null && q ==~ /$defaultFilterExclusionPattern/)){
 //            requestParams.fq = grailsApplication.config.facets.defaultFilters.toString().split(',')
 //            requestParams.fq = ['occurrence_status:"present"', '-user_assertions:(50001 OR 50005 OR 50006)']
             params.put('fq',grailsApplication.config.facets.defaultFilters.toString().split(','));
