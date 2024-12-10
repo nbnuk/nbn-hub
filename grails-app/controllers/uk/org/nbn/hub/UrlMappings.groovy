b3160c00987f74af4f3a763857baacd37d1f721e
Keith Raven <k.raven@nbn.org.uk>
2024-12-10 15:08:31 +0000

    remove redundant routes

--- grails-app/controllers/uk/org/nbn/hub/UrlMappings.groovy
+++ grails-app/controllers/uk/org/nbn/hub/UrlMappings.groovy
@@ -12,8 +12,6 @@ class UrlMappings {
 
         "/"(controller: 'home')
         "/advancedSearch/searchByOccurrenceID"(controller: 'occurrenceSearch', action: 'searchByOccurrenceID')
-        "/occurrences/searchByEventID"(controller: 'occurrenceSearch', action: 'searchByEventID')
-        "/occurrences/searchByCollectionCode"(controller: 'occurrenceSearch', action: 'searchByCollectionCode')
         "/advancedSearch/searchByOther"(controller: 'occurrenceSearch', action: 'searchByOther')
         "/accessControl/filterEditor/$dpuid/$filterUserId/$filterId?"(controller: 'accessControl', action: 'filterEditor')
         "500"(view:'/error')
