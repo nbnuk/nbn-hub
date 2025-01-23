ffda9f05b91e1402621c57dd47ce1ab69a52a61d
h-m-jones <h.manders.jones@nbn.org.uk>
2025-01-23 17:05:30 +0000

    Advandced search updates - ported from develop (legacy) branch

--- src/main/groovy/uk/org/nbn/hub/AdvancedSearchParams.groovy
+++ src/main/groovy/uk/org/nbn/hub/AdvancedSearchParams.groovy
@@ -57,7 +57,7 @@ class AdvancedSearchParams implements Validateable {
     String habitatTaxon = ""
     String eventID = ""
     String collectionCode = ""
-
+    String occurrenceStatus = ""
 
     private String taxa = ""
     private final String QUOTE = "\""
@@ -65,6 +65,8 @@ class AdvancedSearchParams implements Validateable {
     private List queryItems = [];
     private List filterQueryItems = [];
 
+    public static final ALL_OPTION = "All"
+    public static final OCCURRENCE_STATUS_OPTIONS = ["Present", "Absent"]
     /**
      * This custom toString method outputs a valid /occurrence/search query string.
      *
@@ -91,7 +93,7 @@ class AdvancedSearchParams implements Validateable {
         addQueryItem(buildTaxonIDQuery(taxonID))
         addQueryItem(buildEventIDQuery(eventID))
         addQueryItem(buildCollectionCodeQuery(collectionCode))
-
+        addFilterQueryItem(buildOccurrenceStatusQuery(occurrenceStatus))
 
         String encodedQ = queryItems.join(" ${BOOL_OP} ").toString().trim()
         String encodedTaxa;
@@ -274,6 +276,9 @@ class AdvancedSearchParams implements Validateable {
     private String buildGridReferenceGB(gridReference) {
         String query = "";
         switch (gridReference.length()) {
+            case 2:
+                query = "grid_ref_100000:" + gridReference;
+                break;
             case 4:
                 query = "grid_ref_10000:" + gridReference;
                 break;
@@ -296,6 +301,9 @@ class AdvancedSearchParams implements Validateable {
     private String buildGridReferenceIrish(gridReference) {
         String query = "";
         switch (gridReference.length()) {
+            case 1:
+                query = "grid_ref_100000:" + gridReference;
+                break;
             case 3:
                 query = "grid_ref_10000:" + gridReference;
                 break;
@@ -368,7 +376,18 @@ class AdvancedSearchParams implements Validateable {
         return collectionCode?"collection_code:\""+collectionCode+"\"":"";
     }
 
+    private String buildOccurrenceStatusQuery(String occurrenceStatus) {
+        String query="";
 
+        if (occurrenceStatus?.equalsIgnoreCase(ALL_OPTION)){
+            query = "occurrence_status:*"
+        }
+        else if(OCCURRENCE_STATUS_OPTIONS*.toLowerCase().contains(occurrenceStatus?.toLowerCase())){
+            query = "occurrence_status:${occurrenceStatus.toLowerCase()}"
+        }
+
+        return query;
+    }
 
 
     private void addQueryItem(String queryString) {
