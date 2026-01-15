dbabb8f67fdee0b0c35391708fef0e378328919d
h-m-jones <h.manders.jones@nbn.org.uk>
2026-01-15 17:22:05 +0000

    FIX ATLASDEV-2364: access-control added license filter
    https://nbnatlas.atlassian.net/browse/ATLASDEV-2364

--- grails-app/controllers/uk/org/nbn/biocache/hubs/AccessControlController.groovy
+++ grails-app/controllers/uk/org/nbn/biocache/hubs/AccessControlController.groovy
@@ -16,7 +16,7 @@ class AccessControlController {
         requestParams.qc="data_provider_uid:"+params.dpuid
 
         Map facetMap = ["Taxon": ["taxon_name"], "Attribution": ["data_resource_uid", "license"], "Occurrence": ["year"], "Location": ["cl256","cl28"], "Sensitive": ["sensitive"]]
-        requestParams.facets = ["data_resource_uid", "taxon_name", "year", "cl256", "cl28", "sensitive"]
+        requestParams.facets = ["data_resource_uid", "license", "taxon_name", "year", "cl256", "cl28", "sensitive"]
 
 
         command.fq ? requestParams.fq = command.fq : null
