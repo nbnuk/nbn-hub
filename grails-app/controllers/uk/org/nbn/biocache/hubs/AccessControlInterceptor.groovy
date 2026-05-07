44be87427206aa903444e94966779002616c2b23
h-m-jones <h.manders.jones@nbn.org.uk>
2026-05-07 15:53:07 +0100

    corrected the accesscontrol inteceptor (user needs ROLE_ADMIN or ROLE_EDITOR)

--- grails-app/controllers/uk/org/nbn/biocache/hubs/AccessControlInterceptor.groovy
+++ grails-app/controllers/uk/org/nbn/biocache/hubs/AccessControlInterceptor.groovy
@@ -5,7 +5,7 @@ class AccessControlInterceptor {
     def authService
 
     boolean before() {
-        if (!authService?.userInRole('ROLE_ADMIN') && !!authService?.userInRole('ROLE_COLLECTION_EDITOR')) {
+        if (!authService?.userInRole('ROLE_ADMIN') && !authService?.userInRole('ROLE_EDITOR')) {
             log.debug "User not authorised to access the page: ${params.controller}/${params.action ?: ''}. Redirecting to index."
             flash.message = "You are not authorised to access the page: ${params.controller}/${params.action ?: ''}."
             redirect(controller: "home", action: "index")
